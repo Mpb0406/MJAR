4fc63f6d70c67bcaec43e2171d13e0efed86f6fa
Mike Bolloskis <mpb0406@gmail.com>
2022-03-09 20:48:42 -0500

    Created basic user routes - get user, register user, login user. Created userSchema

--- backend/Models/TrainingDayModel.js
+++ backend/Models/TrainingDayModel.js
@@ -1,37 +1,44 @@
 const mongoose = require("mongoose");
 
-const trainingDaySchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: User,
-  },
-  day: {
-    day: {
-      type: Number,
+const trainingDaySchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref: User,
     },
-    lift: [
-      {
-        exercise: {
-          type: String,
-          required: true,
-        },
-        sets: [
-          {
-            reps: {
-              type: Number,
-              required: true,
-            },
-            rpe: {
-              type: Number,
-            },
-            setType: {
-              type: String,
-            },
-          },
-        ],
+    day: {
+      day: {
+        type: Number,
+        required: true,
       },
-    ],
+      lift: [
+        {
+          exercise: {
+            type: String,
+            required: true,
+          },
+          sets: [
+            {
+              reps: {
+                type: Number,
+                required: true,
+              },
+              rpe: {
+                type: Number,
+              },
+              setType: {
+                type: String,
+              },
+            },
+          ],
+        },
+      ],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("TrainingDay", trainingDaySchema);
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/me", (req, res) => {
+  res.send("Get User Info");
+});
+
+router.post("/", (req, res) => {
+  res.send("Register User");
+});
+
+router.post("/login", (req, res) => {
+  res.send("Login User");
+});
+
 module.exports = router;
