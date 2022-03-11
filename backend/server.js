6202acec00cae9c6d31c9ca014ca8cae2ad827c2
Mike Bolloskis <mpb0406@gmail.com>
2022-03-10 20:49:19 -0500

    Created error middleware to throw errors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const { errorHandler } = require("./Middleware/errorMiddleware");
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const port = process.env.PORT || 5000;
 
 //Routes
 app.use("/api/users", require("./Routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
