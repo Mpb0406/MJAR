597b88df008f91ed8ecc5424e6e8e33a1c6d5e11
Mike Bolloskis <mpb0406@gmail.com>
2022-03-31 20:17:23 -0400

    Added Login Functionality to Login Component as well as useEffect and Loader

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.scss";
@@ -19,7 +19,19 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user } = useSelector((state) => state.auth);
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) =>
     setFormData((prevState) => ({
@@ -27,8 +39,21 @@ const Login = () => {
       [e.target.name]: e.target.value,
     }));
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <Form.Group className="d-flex flex-column flex-column align-items-center mt-5">
         <h3 className="text-light dis-font fs-1 mb-4">Login</h3>
         <Form.Control
@@ -49,9 +74,11 @@ const Login = () => {
           value={password}
           onChange={onChange}
         />
-        <Button className="btn-primary mt-4 btn-lg">Login</Button>
+        <Button type="submit" className="btn-primary mt-4 btn-lg">
+          Login
+        </Button>
       </Form.Group>
-    </>
+    </form>
   );
 };
 
