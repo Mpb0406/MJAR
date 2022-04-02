66ac6885afadbfd9ec3f3121dcbd22cd94ddb8e6
Mike Bolloskis <mpb0406@gmail.com>
2022-04-02 18:16:47 -0400

    Finished styling Week Card and Block Page

--- frontend/src/Components/WeekCard.js
+++ frontend/src/Components/WeekCard.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import block from "../img/block.png";
+import week from "../img/week.png";
 
 const WeekCard = () => {
   return (
     <Card className="my-3" bg="primary">
-      <Card.Body className="bg-primary text-light position-relative ">
+      <Card.Body className="bg-primary text-light">
         <Card.Title className=" fs-4 z-10 text-drop-shadow bg-none">
-          Hypertrophy Block
+          Week 1
         </Card.Title>
-        <Card.Text className="bg-primary z-10">4 Weeks Logged</Card.Text>
+        <Card.Text className="bg-primary z-10">
+          5 Days Logged <br />
+          <Card.Text className="bg-primary z-10 fs-small text-info fw-bold">
+            2/21/22 - 2/27/22
+          </Card.Text>
+        </Card.Text>
+
         <Link
-          to="/blockId"
+          to="/weekId"
           className="text-decoration-none hover-secondary hover-secondary bg-primary fs-5">
           View/Edit
         </Link>
         <img
-          src={block}
+          src={week}
           className="h-50 position-absolute absolute-center z-5"
           alt=""
         />
