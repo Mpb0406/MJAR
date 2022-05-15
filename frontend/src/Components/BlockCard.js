44f9a30a1db5330d06486029805727e728d94943
Mike Bolloskis <mpb0406@gmail.com>
2022-05-15 19:48:56 -0400

    Changed Styling on Block Card and Added Popover to Delete - Need to Implement Delete Function

--- frontend/src/Components/BlockCard.js
+++ frontend/src/Components/BlockCard.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
-import { Card, Container, Col, Row, Button } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Col,
+  Row,
+  Popover,
+  OverlayTrigger,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import strength from "../img/strength.png";
+import enterButton from "../img/enterButton.png";
 import DeleteBlockModal from "../Modals/DeleteBlockModal";
 import Moment from "react-moment";
 
@@ -10,50 +18,64 @@ const BlockCard = ({ name, weeks, id, startDate, endDate }) => {
 
   const handleOpen = () => setShow(true);
 
+  const popover = (
+    <Popover id="delete-popover">
+      <Popover.Body className="bg-none py-1 px-0">
+        <h3 className="bg-none fs-6 px-5 py-3 mb-0 cursor-pointer popover-delete-hover">
+          Delete
+        </h3>
+      </Popover.Body>
+    </Popover>
+  );
+
   return (
-    <Card
-      className="d-flex flex-row align-items-center my-3 border-2 border-info"
-      bg="dark"
-      role="button">
+    <Card className="d-flex flex-row align-items-center my-3" bg="info">
       <Container>
         <Row>
-          <Col xs={3} md={2} lg={1}>
-            <img
-              src={strength}
-              className="ms-3 me-5 my-3 card-img flex-grow-3"
-              alt=""
-            />
+          <Col xs={2} md={1} className="bg-info d-flex align-items-center">
+            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+              <h3 className="text-light bg-none ms-3 fs-2 cursor-pointer">⫶</h3>
+            </OverlayTrigger>
           </Col>
-          <Col>
-            <Card.Body className="bg-none text-light">
-              <Card.Title className="d-flex flex-column align-items-start justify-content-between my-0">
-                <h3 className="fs-4 hover-primary">{name}</h3>
-                <h4 className="fs-6">{`${weeks.length} ${
-                  weeks.length === 1 ? "Week" : "Weeks"
-                } Logged`}</h4>
+          <Col md={8} xs={7} className="bg-info ps-0">
+            <Card.Body className="bg-info text-light ps-0">
+              <Card.Title className="d-flex flex-column align-items-start justify-content-between bg-info my-0">
+                <h3 className="fs-3 hover-primary bg-info">{name}</h3>
+                <h4 className="bg-info fs-6 text-sm-center">
+                  <Moment className="bg-info" format="MM/DD/YY">
+                    {startDate}
+                  </Moment>{" "}
+                  -{" "}
+                  <Moment className="bg-info" format="MM/DD/YY">
+                    {endDate}
+                  </Moment>
+                </h4>
+                <h4 className="fs-6 bg-secondary py-1 px-2 rounded-pill">{`${
+                  weeks.length
+                } ${weeks.length === 1 ? "Week" : "Weeks"}`}</h4>
               </Card.Title>
             </Card.Body>
           </Col>
-          <Col>
-            <Card.Body className="bg-none text-light text-center ms-4">
-              <Card.Text className="d-flex flex-column align-items-xs-center align-items-md-end justify-content-between my-0">
-                <h4 className="fs-6 text-sm-center">
-                  <Moment format="MM/DD/YY">{startDate}</Moment> -{" "}
-                  <Moment format="MM/DD/YY">{endDate}</Moment>
-                </h4>
-                <div className="button-wrapper d-flex align-items-center justify-content-center gap-2">
-                  <Button variant="primary rounded-pill d-flex align-items-center justify-content-center mt-1">
+          <Col xs={3} md={3} className="bg-info d-flex align-items-center m-0">
+            <Card.Body className="bg-info text-light text-center ms-0">
+              <Card.Text className="bg-info d-flex flex-column align-items-xs-center align-items-md-end justify-content-between my-0">
+                <div className="bg-info button-wrapper d-flex align-items-center justify-content-center gap-2">
+                  <p className="bg-info rounded-pill d-flex align-items-center justify-content-center mt-1">
                     <Link
                       to={`/training/${id}`}
                       className="text-decoration-none bg-none text-light px-2">
-                      View
+                      <img
+                        className="bg-none enter-button"
+                        src={enterButton}
+                        alt=""
+                      />
                     </Link>
-                  </Button>
-                  <Button
+                  </p>
+                  {/* <Button
                     variant="secondary rounded-pill d-flex align-items-center justify-content-center mt-1"
                     onClick={handleOpen}>
                     ⨉
-                  </Button>
+                  </Button> */}
                 </div>
               </Card.Text>
             </Card.Body>
