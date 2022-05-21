3072e57cc816e5adfd9233f57d2da4753b2ec9f8
Mike Bolloskis <mpb0406@gmail.com>
2022-05-21 17:09:24 -0400

    Prepare Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
@@ -17,6 +18,21 @@ app.use("/api/users", require("./Routes/userRoutes"));
 app.use("/api/training", require("./Routes/trainingDayRoutes"));
 app.use("/api/trainingweeks", require("./Routes/trainingWeekRoutes"));
 app.use("/api/trainingblocks", require("./Routes/trainingBlockRoutes"));
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set Static Assets Folder Which Is What We Get After Running NPM Run Build in React
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  // Get Any Route From index.html File Located In Build Folder
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please Set to Production"));
+}
 
 app.use(errorHandler);
 
